5ad9a0b2b1346e5d6ba95971fda99eb1dd5b30d2
Samih Freire <samihcisne@gmail.com>
2022-03-17 22:10:20 -0300

    Aprendizado de novas funcionalidades Alura: 06.Construtores e membros estáticos

---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,58 @@
+﻿//using _05_ByteBank; --Tornas-se desnecessário a utilização desse using pois ja foi defenido: namespace _06_ByteBank
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        //private _06_ByteBank.Cliente _titular;   //Como mencionado acima, caso queira posso retirar o namespace _06_ByteBank pois já criado em cima
+        public Cliente Titular { get; set; }       //Como nao existe uma lógica realizando atividades, basta simplificar dessa forma
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        
+        private double _saldo = 100;               //Utilizo _ para delimitar se é uma variavel privada, para facilitar o reconhecimento
+
+        public double Saldo
+        {
+            get 
+            { 
+                return _saldo; 
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+        }
+        public bool Sacar(double valor)
+        {
+            if(this._saldo < valor)
+            {
+                return false;
+            }
+            this._saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if(this._saldo < valor)
+            {
+                return false;
+            }
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.nome = "Guilherme";
+            cliente.CPF = "434.564.879-20";
+            cliente.profissao = "Desenvolvedor C#";
+
+            conta.Titular = cliente;
+            conta.Agencia = 563;
+            conta.Numero = 56321855;
+            conta.Saldo = 4900.31;
+
+            Console.WriteLine("Cliente: " + conta.Titular.nome);
+            Console.WriteLine("Agencia: " + conta.Agencia);
+            Console.WriteLine("Número: " + conta.Numero);
+            Console.WriteLine("Saldo: " + conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
